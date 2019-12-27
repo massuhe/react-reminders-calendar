d53d7d68ab8fce28fd50691a49eb6520ba5c7710
Esteban Massuh <massuhe@outlook.com>
2019-12-27 13:03:46 -0300

    build: setup dev environment

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+import App from './App'
+
+const root = document.getElementById('root')
+
+ReactDOM.render(<App />, root)
